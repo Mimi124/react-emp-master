1c069ed8fd24be281518ceab35c930949196a116
Mimi124 <miriamamoako98@gmail.com>
2022-06-21 21:30:12 +0000

    init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const useStyles = makeStyles({
@@ -13,7 +14,12 @@ function Home() {
   const classes = useStyles();
   return (
     <div className={classes.homePage}>
-        This is the main page
+        <h1>This is the Dashboard</h1>
+
+        <h4>
+    Here is a link to view  and add Employees
+    <Link to="/table"> Click Me </Link>
+    </h4>
     </div>
   );
 }
